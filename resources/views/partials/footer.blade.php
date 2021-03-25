b9d2e1721bdaff0a3000083b55a4acdfcde65f94
Irakoze Maurice <iramaurimade@gmail.com>
2021-03-25 13:28:30 +0200

    desktop pages and styles

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,10 @@
+<footer>
+    <div class="footer-content container">
+        <div class="made-with">Made with <i class="fa fa-heart heart"></i> by Irakoze H M</div>
+        <ul>
+            <li>Follow Me: </li>
+            <li><a href="https://github.com/IrakozeMaurice"><i class="fa fa-github"></i></a></li>
+            <li><a href="#"><i class="fa fa-twitter"></i></a></li>
+        </ul>
+    </div> <!-- end footer-content -->
+</footer>
---
+++ routes/web.php
@@ -0,0 +1,13 @@
+<?php
+
+Route::view('/', 'homepage');
+
+Route::view('/products', 'products');
+
+Route::view('/product', 'product');
+
+Route::view('/cart', 'cart');
+
+Route::view('/checkout', 'checkout');
+
+Route::view('/thankyou', 'thankyou');

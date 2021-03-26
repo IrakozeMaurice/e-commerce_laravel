255ec538af8d3e70f0783e1f030c26e1f6fc0cb8
Irakoze Maurice <iramaurimade@gmail.com>
2021-03-26 10:58:40 +0200

    all pages with dynamic data

---
+++ app/Http/Controllers/ShopController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Product;
+use Illuminate\Http\Request;
+
+class ShopController extends Controller
+{
+
+    public function index()
+    {
+        $products = Product::inRandomOrder()->take(12)->get();
+
+        return view('shop', compact('products'));
+    }
+
+    public function show($slug)
+    {
+        $product = Product::where('slug', $slug)->firstOrFail();
+        $mightAlsoLike = Product::where('slug', '!=', $slug)->inRandomOrder()->take(4)->get();
+
+        return view('product', compact('product', 'mightAlsoLike'));
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,14 @@
 <?php
 
-Route::view('/', 'homepage');
+// Route::view('/', 'landing-page');
 
-Route::view('/products', 'products');
+Route::get('/', 'LandingPageController@index')->name('landing-page');
+
+// Route::view('/shop', 'shop');
+
+Route::get('/shop', 'ShopController@index')->name('shop.index');
+
+Route::get('/shop/{product}', 'ShopController@show')->name('shop.show');
 
 Route::view('/product', 'product');
 
